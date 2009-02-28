fcbdd8155634f574af54d4a2a2fa32de8a76eeb5
Kanru Chen <koster@debian.org.tw>
2009-02-28 20:04:10 +0800

    Add usage

--- tongwen.py
+++ tongwen.py
@@ -4,6 +4,10 @@
 from mcache import *
 import sys
 
+def usage():
+    print 'Usage: tongwen.py [-s2t] inputfile'
+    exit(0)
+
 def dbpath():
     import os.path, sys
     if __name__ == '__main__':
@@ -12,24 +16,39 @@ def dbpath():
         filename = __file__
     return os.path.abspath(os.path.dirname(filename))
 
+if len(sys.argv) < 2:
+    usage()
+
 s2t = mcache(dbpath()+'/s2t.db')
 t2s = mcache(dbpath()+'/t2s.db')
 s2tp = mcache(dbpath()+'/s2t_p.db')
 t2sp = mcache(dbpath()+'/t2s_p.db')
 
-f = file(sys.argv[1])
+tb1 = s2t
+tb2 = s2tp
+
+inputf = sys.argv[1]
+if inputf == '-t2s':
+    if len(sys.argv) < 3:
+        usage()
+    else:
+        tb1 = t2s
+        tb2 = t2sp
+        inputf = sys.argv[2]
+
+f = file(inputf)
 lines = f.readlines()
 all = ''.join(lines).decode('UTF-8')
 copy = []
 
 for c in all:
-    if s2t.db.has_key(c.encode('UTF-8')):
+    if tb1.db.has_key(c.encode('UTF-8')):
         copy.append(s2t.db[c.encode('UTF-8')])
     else:
         copy.append(c.encode('UTF-8'))
 
 out = ''.join(copy)
-for k, v in s2tp.db.iteritems():
+for k, v in tb2.db.iteritems():
     if v != '':
         out = out.replace(k, v)
 print out
